23ccc2801a06b7379c8a7c248df54876adb052a8
Volt-Prime <63620906+Volt-Prime@users.noreply.github.com>
2022-01-23 21:04:36 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '叔叔很生气';
-    if (score <= 99) return '你所热爱的就是你的生活';
-    if (score <= 149) return '您就是蒙古上单？';
-    if (score <= 199) return '您马什么时候4啊？';
+    if (score <= 49) return '寒假加作业！';
+    if (score <= 99) return '不许上课睡觉';
+    if (score <= 149) return '佬？';
+    if (score <= 199) return '可以免寒假作业了';
     return '人？';
 }
 
